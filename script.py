9f46dc011344b7ada325693d49c59ca54c60437b
toyto <anko@unisport.dk>
2019-12-06 16:43:53 +0200

    Selected data for patients

--- script.py
+++ script.py
@@ -1,12 +1,19 @@
 import json
 import requests
 from urllib import urlopen
-from pyspark import SparkContext, sqlContext
+from pyspark import SparkContext
+from pyspark.sql.functions import (
+    col,
+    to_date,
+    to_timestamp,
+    lit,
+    round as _round,
+)
 
 
 DB_URL = 'jdbc:postgresql://{db_host}:{db_port}/{database}?user={db_user}'.format(
-    db_host='docker.for.mac.localhost', 
-    db_port=5432, 
+    db_host='docker.for.mac.localhost',
+    db_port=5432,
     database='healthdb',
     db_user='andrew',
 )
@@ -16,6 +23,7 @@ db_data = spark.read.format('jdbc').options(
     url=DB_URL, dbtable='patient', driver='org.postgresql.Driver'
 ).load()
 
+
 def parse_dataframe(json_data):
     mylist = []
     for line in json_data.splitlines():
@@ -23,6 +31,7 @@ def parse_dataframe(json_data):
     rdd = sc.parallelize(mylist)
     df = sqlContext.read.json(rdd)
     return df
+
 
 def df_from_url(url):
     response = urlopen(url)
@@ -38,3 +47,36 @@ observation_df = df_from_url(url_observation)
 patient_df = df_from_url(url_patient)
 procedure_df = df_from_url(url_procedure)
 encounter_df = df_from_url(url_encounter)
+
+patients_by_races = patient_df.select(
+    col('id').alias('source_id'),
+    to_date('birthDate').alias('birth_date'),
+    col('gender'),
+    col('address')[0]['country'].alias('country'),
+    col('extension')['valueCodeableConcept'][
+        'coding'][0]['code'][0].alias('race_code'),
+    col('extension')['valueCodeableConcept']['coding'][
+        0]['system'][0].alias('race_code_system'),
+).filter(
+    col('extension')['url'][
+        0] == 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-race',
+)
+
+patients_by_ethnicity = patient_df.select(
+    col('id').alias('source_id_dummy'),
+    col('extension')['valueCodeableConcept']['coding'][
+        0]['code'][0].alias('ethnicity_code'),
+    col('extension')['valueCodeableConcept']['coding'][
+        0]['system'][0].alias('ethnicity_code_system'),
+).filter(
+    col('extension')['url'][
+        0] == 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity',
+)
+
+patients = patients_by_races.join(
+    patients_by_ethnicity,
+    patients_by_races.source_id == patients_by_ethnicity.source_id_dummy,
+    how='left_outer'
+).drop(col('source_id_dummy'))
+
+patients.show(10)
